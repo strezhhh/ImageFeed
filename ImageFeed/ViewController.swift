ae616f0153c8900b63d2ee3c8d3a4b820a96ddf7
Pavel Strezhnev <pstrej@yandex.ru>
2026-09-08 21:55:47 +0300

    Создан пустой проект ImageFeed

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Strezh on 05.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
