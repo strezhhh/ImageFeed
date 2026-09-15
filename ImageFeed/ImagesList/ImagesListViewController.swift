1a3bee463fa223345b183720c9bcd7958bbd2bad
Pavel Strezhnev <pstrej@yandex.ru>
2026-09-15 21:59:23 +0300

    add a zero check to the imageWidth variable

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -77,6 +77,9 @@ extension ImagesListViewController: UITableViewDelegate {
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
+        guard imageWidth != 0 else {
+            return 0
+        }
         let scale = imageViewWidth / imageWidth
         let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
         return cellHeight
